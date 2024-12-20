08e2607a56eb180f74abaa23e81ffcf6034e8906
Aung Kyaw Soe <33275086+ArjunKarki@users.noreply.github.com>
2024-12-20 20:33:45 +0700

    add dummy joblisting section

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <Navbar />
   <Hero />
+  <HomeCard />
+  <JobListings />
 </template>
 
 <script setup>
 import Hero from "./components/Hero.vue";
+import HomeCard from "./components/HomeCard.vue";
+import JobListings from "./components/JobListings.vue";
 import Navbar from "./components/Navbar.vue";
 </script>
---
+++ src/components/JobListings.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="px-4 py-10 bg-green-10">
+    <h2 class="text-3xl font-bold text-center text-green-500 mb-6">
+      Browse Jobs
+    </h2>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div v-for="job in jobs" :key="job.id">
+        <JobListing />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import jobData from "../jobs.json";
+import JobListing from "./JobListing.vue";
+
+const jobs = ref(jobData.jobs);
+</script>
